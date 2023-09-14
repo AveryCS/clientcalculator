5c58134ff0e6a4519d067ec5008f0b7ecf964115
AveryCS <averydcs@gmail.com>
2023-09-14 16:15:54 -0400

    began method for calculateClientRating in the ClientRateingCalculator class

--- src/main/java/com/studiofaye/clientcalculator/calculators/ClientRatingCalculator.java
+++ src/main/java/com/studiofaye/clientcalculator/calculators/ClientRatingCalculator.java
@@ -1,4 +1,20 @@
 package com.studiofaye.clientcalculator.calculators;
+import java.util.*;
+import static java.lang.Math.round;
 
 public class ClientRatingCalculator {
+
+    public ClientRatingCalculator() {
+    }
+
+
+
+    public int calculateClientRating(int hoursBookedPerYearPoints, int hourlyRatePoints,int easeToWorkWith){
+//     int hoursBookedTotal=  (int) Float.valueOf(hoursBookedPerYearPoints*.25);
+
+
+    return 0;
+    }
+
+
 }
