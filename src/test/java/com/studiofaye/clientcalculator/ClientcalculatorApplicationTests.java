9590428637a3954e941c9b3b31a68958822d02f8
AveryCS <averydcs@gmail.com>
2023-10-04 12:10:42 -0400

    Autowired an instance of the percentile points calculator in the test class

--- src/test/java/com/studiofaye/clientcalculator/ClientcalculatorApplicationTests.java
+++ src/test/java/com/studiofaye/clientcalculator/ClientcalculatorApplicationTests.java
@@ -3,7 +3,6 @@ package com.studiofaye.clientcalculator;
 import com.studiofaye.clientcalculator.calculators.PercentilePointsCalculator;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -11,14 +10,12 @@ class ClientcalculatorApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(0,0);
 	}
 
-	@Test
-	public void testPercentilePointsCalculator(){
-		PercentilePointsCalculator calculator = new PercentilePointsCalculator();
-		int points = calculator.mapValueToNumber(25, 0, 100);
-		assertEquals(3, points);
-	}
+
+
+
 }
 
 
--- src/test/java/com/studiofaye/clientcalculator/calculators/PercentilePointsCalculatorTest.java
+++ src/test/java/com/studiofaye/clientcalculator/calculators/PercentilePointsCalculatorTest.java
@@ -1,16 +1,29 @@
 package com.studiofaye.clientcalculator.calculators;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class PercentilePointsCalculatorTest {
+@Autowired PercentilePointsCalculator calculator;
 
     @Test
-    public void testPercentilePointsCalculator() {
-        PercentilePointsCalculator calculator = new PercentilePointsCalculator();
+    public void testMapValueToNumber_WithValidInput(){
         int points = calculator.mapValueToNumber(25, 0, 100);
-        assertEquals(2, points);
+        assertEquals(3, points);
+    }
+
+    @Test
+    public void testMapValueToNumber_WithTooHighValue(){
+        int points = calculator.mapValueToNumber(102, 0, 100);
+        assertEquals(10, points);
+    }
+
+    @Test
+    public void testMapValueToNumber_WithNegativeValue(){
+        int points = calculator.mapValueToNumber(-5, 0, 100);
+        assertEquals(1, points);
     }
 }
